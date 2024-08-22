dbdd68cedcb43565dea92113357cbf03867f746a
Sarah Nicacio <nicaciosarah085@gmail.com>
2024-08-21 22:12:23 -0300

    first commit

--- junit5-jupiter-starter-maven/src/main/java/com/example/project/Calculator.java
+++ junit5-jupiter-starter-maven/src/main/java/com/example/project/Calculator.java
@@ -15,5 +15,14 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
+	public int mult(int a, int b) {
+		return a * b;
+	}
 
 }
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
@@ -14,28 +14,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
 
 	@Test
-	@DisplayName("1 + 1 = 2")
-	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
-	}
-
-	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@CsvSource({
-			"0,    1,   1",
-			"1,    2,   3",
-			"49,  51, 100",
-			"1,  100, 101"
-	})
-	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
-	}
+    @DisplayName("1 - 1 = 0")
+    void substractTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.sub(1, 1), "1 - 1 should equal 0");
+    }
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addsTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+    @Test
+    @DisplayName("3 x 3 = 9")
+    void multiplyTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(9, calculator.mult(3, 3), "3 x 3 should equal 9");
+    }
+    @Test
+    @DisplayName("4 / 2 = 2")
+    void divTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.div(4, 2), "4 / 2 should equal 2");
+    }
 }
